954f8ded15dd6764904f6eac9d614c83f9afa59d
chahn0711 <chahn0711@naver.com>
2023-10-24 15:07:15 +0900

    20231024오후

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { Fragment } from "react";
-import './App.css';
+import "./App.css";
 import Section from "./Section";
 import MyComponent from "./MyComponent";
-
+import Say from "./Say";
 
 // function App() { // 부모
 //   const element = < WelcomeProp name="곰돌이사육사" /> // 컴퍼런트 호출 방법<>
-//   return ( // 필히 하나의 태그로 묶어주어야한다. <>태그 쓰거나 <Fragment>태그 
+//   return ( // 필히 하나의 태그로 묶어주어야한다. <>태그 쓰거나 <Fragment>태그
 //     <>
 //       <div>{element}</div>
 //     </>
@@ -17,7 +17,10 @@ function App() {
     <>
       {/* <Section title="스포츠" content="This is the content for section 1." />      
       <Section title="정치" content="This is the content for section 2." />       */}
-      <MyComponent name="곰돌이사육사" age={30}/>
+      {/* <MyComponent name="곰돌이사육사" age={30}>
+        안녕하세요^^
+      </MyComponent> */}
+      <Say />
     </>
   );
 }
--- src/Section.jsx
+++ src/Section.jsx
@@ -2,22 +2,23 @@
 import bgImg from "../src/images/bg2.jpg";
 
 function Heading(props) {
-    return <h1>{props.text}</h1>
+  return <h1>{props.text}</h1>;
 }
 
 function Paragraph(props) {
-    return <p>{props.text}</p>
+  return <p>{props.text}</p>;
 }
 function ImgUrl(props) {
-    return <img style={{height: "300px"}}src={bgImg} alt="이미지 테스트"/>
+  return <img style={{ height: "300px" }} src={bgImg} alt="이미지 테스트" />;
 }
 
-const Section = (props) => { // 컴포넌트는 함수랑 구분하기 위해 앞문자는 대문자로 쓴다.
-    return(
-        <>
-            <Heading text={props.title} />
-            <Paragraph text={props.content} />
-            <ImgUrl url={props.url}/>
-        </>
-    );
-}
+const Section = (props) => {
+  // 컴포넌트는 함수랑 구분하기 위해 앞문자는 대문자로 쓴다.
+  return (
+    <>
+      <Heading text={props.title} />
+      <Paragraph text={props.content} />
+      <ImgUrl url={props.url} />
+    </>
+  );
+};
